c1497404353a3088cd878c7a7e6fbeb729c22faa
Caged <justin@labratrevenge.com>
2018-08-12 20:30:05 -0700

    add explicit count and remove wobbles

--- grid-of-grids.js
+++ grid-of-grids.js
@@ -1,32 +1,32 @@
 const canvasSketch = require('canvas-sketch')
-const palettes = require('nice-color-palettes/100.json')
+const palettes = require('nice-color-palettes/500.json')
 const shuffle = require('shuffle-array')
 const {scaleBand} = require('d3-scale')
 const {range} = require('d3-array')
 const seedrandom = require('seedrandom')
-const {shaky} = require('./vendor/shaky')
 
 const settings = {
   dimensions: 'A4',
   pixelRatio: devicePixelRatio,
-  exportPixelRatio: 2
+  exportPixelRatio: devicePixelRatio
 }
+
+const count = 100
 
 canvasSketch(() => {
   return ({context: ctx, width, height}) => {
     const seed = Date.now()
     /* eslint-disable-next-line */
     console.log(`Generating with seed ${seed}`)
-    const sctx = shaky.create(ctx.canvas)
     const rng = seedrandom(seed)
-    const xys = Math.ceil(Math.sqrt(palettes.length))
+    const xys = Math.ceil(Math.sqrt(count))
     const y = scaleBand()
       .domain(range(0, xys))
       .range([0, height])
       .padding(0.0, 0.0)
       .round(true)
     const x = scaleBand()
-      .domain(range(0, palettes.length / xys))
+      .domain(range(0, count / xys))
       .range([0, width])
       .round(true)
 
@@ -34,13 +34,11 @@ canvasSketch(() => {
     ctx.fillStyle = '#fff'
     ctx.fillRect(0, 0, width, height)
 
-    let j = 1
-    shaky.shake = j
     for (const yv of y.domain()) {
       for (const xv of x.domain()) {
         const cellWidth = x.bandwidth()
         const cellHeight = y.bandwidth()
-        const rings = 5
+        const rings = 6
         const step = Math.floor(Math.min(cellWidth, cellHeight) / 2 / rings)
         const colors = shuffle.pick(palettes, {rng})
         let pick = -1
@@ -54,18 +52,16 @@ canvasSketch(() => {
           const rx = x(xv) + step * i
           const ry = y(yv) + step * i
           ctx.strokeStyle = colors[(pick = ++pick >= colors.length ? 0 : pick)]
-          sctx.beginPath()
-          sctx.rect(rx, ry, xsize, ysize)
-          sctx.stroke()
+          ctx.beginPath()
+          ctx.rect(rx, ry, xsize, ysize)
+          ctx.stroke()
         }
-        ++j
 
         // ctx.strokeStyle = '#333'
         // ctx.lineWidth = 1
-        //
-        // sctx.beginPath()
-        // sctx.rect(x(xv), y(yv), x.bandwidth(), y.bandwidth())
-        // sctx.stroke()
+        // ctx.beginPath()
+        // ctx.rect(x(xv), y(yv), x.bandwidth(), y.bandwidth())
+        // ctx.stroke()
       }
     }
   }
