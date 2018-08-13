2e47ef4b3a044cea38f1b33d6e1858bcd599046f
Caged <justin@labratrevenge.com>
2018-08-12 17:58:42 -0700

    rotated rectangles

--- hello-world.js
+++ hello-world.js
@@ -1,34 +1,37 @@
 const canvasSketch = require('canvas-sketch')
+const palettes = require('nice-color-palettes')
+const shuffle = require('shuffle-array')
 
 const settings = {
-  dimensions: [500, 500],
+  dimensions: [1000, 1000],
   pixelRatio: devicePixelRatio,
   exportPixelRatio: 2
 }
 
-function hdpi(canvas, ctx, width, height) {
-  canvas.width = width * devicePixelRatio
-  canvas.height = height * devicePixelRatio
-  canvas.style.width = `${width}px`
-  canvas.style.height = `${height}px`
-  ctx.scale(devicePixelRatio, devicePixelRatio)
-}
-
 const sketch = () => {
   return ({context: ctx, width, height}) => {
-    ctx.clearRect(0, 0, width, height)
-    // hdpi(ctx.canvas, ctx, width, height)
-    ctx.fillStyle = 'white'
-    ctx.strokeStyle = 'black'
-    ctx.lineWidth = 1
+    const rings = 25
+    width -= rings * 2
+    height -= rings * 2
+    const step = Math.floor(width / 2 / rings)
+    const colors = shuffle.pick(palettes)
+    let pick = -1
 
-    const rings = width / 50
-    const step = 25
-    for (let i = 0; i < rings; i++) {
+    ctx.clearRect(0, 0, width, height)
+    ctx.lineWidth = step * 0.5
+
+    for (let i = 1; i <= rings; i++) {
       const size = width - step * i * 2
+      const x = step * i + rings
+      const y = step * i + rings
+      ctx.strokeStyle = colors[(pick = ++pick >= colors.length ? 0 : pick)]
+      ctx.save()
       ctx.beginPath()
-      ctx.rect(step * i, step * i, size, size)
+      ctx.translate(x + width / 2, y + height / 2)
+      ctx.rotate((5 * Math.PI) / 180)
+      ctx.rect(-width / 2, -height / 2, size, size)
       ctx.stroke()
+      ctx.restore()
     }
   }
 }
