ac22f6d0d0538004e4ad5dd560dc6d5f78754ca9
Caged <justin@labratrevenge.com>
2018-08-19 09:13:13 -0700

    further tweaks to ltree instructions

--- ltree.js
+++ ltree.js
@@ -4,52 +4,36 @@ const shuffle = require('shuffle-array')
 const {palette} = require('./utils/palette')
 
 const settings = {
-  dimensions: [2048, 2048]
+  dimensions: 'A3',
+  pixelRatio: devicePixelRatio,
+  exportPixelRatio: devicePixelRatio
 }
 
-const lineIncrement = 2
-
+const lineIncrement = 100
 const palettes = palette()
 const pcolors = palettes.random()
 const baseColor = shuffle.pick(pcolors)
 const colors = pcolors.filter(c => c !== baseColor)
 
-const sketch = ({context: ctx, width, height}) => {
-  let linelength = 180
+const sketch = ({context: ctx, width}) => {
+  let linelength = lineIncrement
   const tree = new LSystem({
     productions: {
       X: {
         successors: [
-          {
-            weight: 0.5,
-            successor: 'FFF-[[X]+X]+F[+FX]-X'
-          },
-          {
-            weight: 0.5,
-            successor: 'FF-[X+]+FF[-FX]-XF[X-]'
-          }
+          {weight: 0.05, successor: 'F[+X]FF-[X+FF-X]+X'},
+          {weight: 0.45, successor: 'F[-X]FF+[X-FF+X]-X'},
+          {weight: 0.05, successor: 'FF-[[X]+X]+F[+FX]-X'},
+          {weight: 0.45, successor: 'FF-[X+]+FF[-FX]-XF[X-]'}
         ]
       },
       F: {
-        successors: [
-          {
-            weight: 0.8,
-            successor: 'F'
-          },
-          {
-            weight: 0.1,
-            successor: 'FFF'
-          },
-          {
-            weight: 0.01,
-            successor: 'FF+'
-          }
-        ]
+        successors: [{weight: 0.8, successor: 'F'}, {weight: 0.1, successor: 'FFF'}, {weight: 0.1, successor: 'FF+'}]
       }
     },
     finals: {
       F: () => {
-        ctx.lineWidth += Math.random() - 0.2
+        ctx.lineWidth += Math.random() * 1
         ctx.beginPath()
         ctx.moveTo(0, 0)
         ctx.lineTo(0, linelength / (tree.iterations + 1))
@@ -57,12 +41,12 @@ const sketch = ({context: ctx, width, height}) => {
         ctx.translate(0, linelength / (tree.iterations + 1))
       },
       '+': () => {
-        ctx.strokeStyle = colors[0]
-        ctx.rotate((Math.PI / 180) * 90)
+        ctx.strokeStyle = shuffle.pick(colors)
+        ctx.rotate((Math.PI / 180) * 45)
       },
       '-': () => {
-        ctx.strokeStyle = colors[1]
-        ctx.rotate((Math.PI / 180) * -90)
+        ctx.strokeStyle = shuffle.pick(colors)
+        ctx.rotate((Math.PI / 180) * -45)
       },
       '[': () => {
         ctx.save()
@@ -72,22 +56,25 @@ const sketch = ({context: ctx, width, height}) => {
       },
       ']': () => {
         ctx.restore()
-        ctx.lineWidth *= 0.7
+        ctx.lineWidth *= 0.5
         linelength += lineIncrement
       }
     }
   })
 
   tree.setAxiom('X')
-  tree.iterate(7)
+  tree.iterate(6)
 
-  return ({context: ctx}) => {
-    ctx.clearRect(0, 0, width, height)
-    ctx.fillStyle = baseColor
+  return ({context: ctx, canvasWidth, canvasHeight}) => {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
     ctx.strokeStyle = colors[0]
-    ctx.fillRect(0, 0, width, height)
-    ctx.translate(width / 2, height / 1.5)
-    ctx.rotate(Math.PI)
+    ctx.fillStyle = baseColor
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight)
+    ctx.fill()
+
+    ctx.translate(width / 2, width / 1.5)
+    ctx.rotate((Math.PI / 180) * shuffle.pick([90, -90, 180, -180]))
     tree.final()
   }
 }
