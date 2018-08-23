d2d56dd1dc39d915e295f50ce532d7599cff0596
Caged <justin@labratrevenge.com>
2018-08-22 18:00:42 -0700

    tweaks to vf lines

--- vector-field-lines.js
+++ vector-field-lines.js
@@ -4,18 +4,18 @@ const {interpolateSinebow} = require('d3-scale-chromatic')
 const {scaleSequential} = require('d3-scale')
 
 const settings = {
-  dimensions: [1500, 1500],
+  dimensions: 'A1',
+  orientation: 'landscape',
   animate: true,
   pixelRatio: devicePixelRatio
 }
 
 const sketch = ({width, height}) => {
   noise.seed(Math.random())
-  const resolution = 20
+  const resolution = 50
   const columns = Math.floor(width / resolution) + 1
   const rows = Math.floor(height / resolution) + 1
-  const color = scaleSequential(interpolateSinebow).domain([(-360 * Math.PI) / 180, (360 * Math.PI) / 180])
-  let noisez = 0
+  const color = scaleSequential(interpolateSinebow).domain([-Math.PI * 2, Math.PI * 2])
 
   // TODO: use typed, flat array
   const field = new Array(columns)
@@ -26,20 +26,18 @@ const sketch = ({width, height}) => {
     }
   }
 
-  return ({context: ctx, width, height}) => {
+  return ({context: ctx, width, height, frame, time}) => {
     ctx.fillStyle = 'black'
     ctx.fillRect(0, 0, width, height)
 
     for (let x = 0; x < columns; x++) {
       for (let y = 0; y < rows; y++) {
-        const angle = noise.simplex3(x / (resolution * 2), y / (resolution * 2), noisez) * Math.PI * 2
-        const length = noise.simplex3(x * 0.01, y * 0.01, noisez)
+        const angle = noise.simplex3(x / (resolution * 2), y / (resolution * 2), time * 0.3) * Math.PI * 2
+        const length = noise.simplex3(x * 0.01, y * 0.01, time * 0.3)
         field[x][y][0] = angle
         field[x][y][1] = length
       }
     }
-
-    noisez += 0.004
 
     for (let x = 0; x < columns; x++) {
       for (let y = 0; y < rows; y++) {
