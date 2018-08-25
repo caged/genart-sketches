5a8d670cd4476f669dcf46840ed4b3e191936646
Caged <justin@labratrevenge.com>
2018-08-25 13:48:13 -0700

    night globe

--- sketches/three-globe.js
+++ sketches/three-globe.js
@@ -1,6 +1,7 @@
 const canvasSketch = require('canvas-sketch')
 
-// Assign THREE to global for the examples/
+const SPHERE_SIZE = 1.0
+let landMesh, cloudMesh, boundariesMesh
 const THREE = require('three')
 global.THREE = THREE
 
@@ -9,8 +10,8 @@ require('three/examples/js/controls/OrbitControls')
 
 // Parameters for the sketch
 const settings = {
-  animate: true, // Ensure we get an animation loop
-  context: 'webgl', // Setup WebGL instead of 2D canvas
+  animate: true,
+  context: 'webgl',
   attributes: {antialias: true} // Turn on MSAA
 }
 
@@ -20,42 +21,105 @@ const sketch = ({context, width, height}) => {
   })
 
   // Setup a 3D perspective camera
-  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100)
+  const camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 100)
   camera.position.set(2, 2, -4)
   camera.lookAt(new THREE.Vector3())
 
   // Orbit controls for click + drag interaction
   const controls = new THREE.OrbitControls(camera)
   const scene = new THREE.Scene()
-  scene.background = new THREE.Color(0x111111)
+  scene.background = new THREE.Color(0x171921)
 
-  const alight = new THREE.AmbientLight(0x444444)
-  scene.add(alight)
+  const axesHelper = new THREE.AxesHelper(25)
+  scene.add(axesHelper)
 
-  const light = new THREE.DirectionalLight(0xffffff, 0.5)
-  light.position.set(width / 2, height / 2, 0.5)
-  scene.add(light)
+  // Setup lights
+  const ambientLight = new THREE.AmbientLight(0xffd28b)
+  scene.add(ambientLight)
 
-  light.castShadow = true
-  light.shadowCameraNear = 0.01
-  light.shadowCameraFar = 15
-  light.shadowCameraFov = 45
-  light.shadowCameraLeft = -1
-  light.shadowCameraRight = 1
-  light.shadowCameraTop = 1
-  light.shadowCameraBottom = -1
-  // light.shadowCameraVisible	= true
-  light.shadowBias = 0.001
-  light.shadowDarkness = 0.2
-  light.shadowMapWidth = width
-  light.shadowMapHeight = height
+  const l3 = new THREE.HemisphereLight(0xffffbb, 0x080820, 3)
+  scene.add(l3)
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+  scene.add(directionalLight)
+
+  directionalLight.castShadow = true
+  const scamera = directionalLight.shadow.camera
+  scamera.near = 0.1
+  scamera.far = 15
+  scamera.fov = 45
+  scamera.left = -1
+  scamera.right = 1
+  scamera.top = 1
+  scamera.bottom = -1
+  directionalLight.shadow.bias = 0.001
+  directionalLight.shadow.darkness = 0.2
+  directionalLight.shadow.mapSize.width = width
+  directionalLight.shadow.mapSize.height = height
 
   // const helper = new THREE.CameraHelper(light.shadow.camera)
   // scene.add(helper)
 
-  // Add a basic ThreeJS mesh
-  const mesh = new THREE.Mesh(new THREE.SphereGeometry(0.9, 32, 32), new THREE.MeshPhongMaterial({wireframe: true}))
-  scene.add(mesh)
+  // const p1 = new THREE.PointLight(0xff00ff, 10, 100)
+  // p1.position.set(0, 0, -50)
+  // scene.add(p1)
+
+  // const backLight = new THREE.DirectionalLight(0xffffff, 4)
+  // backLight.position.set(-0.5, -1, 1)
+  // backLight.lookAt(scene.position)
+  // scene.add(backLight)
+  // const helper = new THREE.DirectionalLightHelper(backLight, 2)
+  // scene.add(helper)
+
+  // const helper = new THREE.CameraHelper(backLight.shadow.camera)
+  // scene.add(helper)
+
+  // const pointLightHelper = new THREE.PointLightHelper(p1, 250)
+  // scene.add(pointLightHelper)
+
+  const geometry = new THREE.SphereGeometry(SPHERE_SIZE, 32, 32)
+  const material = new THREE.MeshPhongMaterial({shininess: 5})
+  const landMesh = new THREE.Mesh(geometry, material)
+
+  material.specular = new THREE.Color(0xafd9ff)
+  material.bumpScale = 0.05
+  material.map = new THREE.TextureLoader().load('../assets/5_night_8k.jpg')
+  material.bumpMap = new THREE.TextureLoader().load('../assets/elev_bump_8k.jpg')
+  material.specularMap = new THREE.TextureLoader().load('../assets/water_8k.png')
+  scene.add(landMesh)
+
+  // const cloudMesh = new THREE.Mesh(
+  //   new THREE.SphereGeometry(SPHERE_SIZE + 0.005, 32, 32),
+  //   new THREE.MeshPhongMaterial({
+  //     map: new THREE.TextureLoader().load('../assets/fair_clouds_8k.png'),
+  //     transparent: true,
+  //     side: THREE.DoubleSide,
+  //     opacity: 0.8
+  //   })
+  // )
+  // scene.add(cloudMesh)
+
+  // const startField = new THREE.Mesh(
+  //   new THREE.SphereGeometry(SPHERE_SIZE * 3, 32, 32),
+  //   new THREE.MeshBasicMaterial({
+  //     map: new THREE.TextureLoader().load('../assets/starfield.jpg'),
+  //     side: THREE.BackSide
+  //   })
+  // )
+  //
+  // scene.add(startField)
+
+  // const boundariesMesh = new THREE.Mesh(
+  //   new THREE.SphereGeometry(SPHERE_SIZE, 32, 32),
+  //   new THREE.MeshPhongMaterial({
+  //     map: new THREE.TextureLoader().load('../assets/boundaries_transparent_8k.png'),
+  //     transparent: true,
+  //     side: THREE.DoubleSide,
+  //     opacity: 0.2
+  //   })
+  // )
+  //
+  // scene.add(boundariesMesh)
 
   return {
     // Handle window resize events
@@ -65,10 +129,13 @@ const sketch = ({context, width, height}) => {
       camera.aspect = viewportWidth / viewportHeight
       camera.updateProjectionMatrix()
     },
+
     // Render each frame
     render({deltaTime}) {
-      // Rotate our mesh slowly
-      mesh.rotation.y += deltaTime * ((10 * Math.PI) / 180)
+      for (const mesh of [landMesh, boundariesMesh, cloudMesh]) {
+        if (mesh) mesh.rotation.y += deltaTime * ((5 * Math.PI) / 180)
+      }
+
       controls.update()
       renderer.render(scene, camera)
     }
