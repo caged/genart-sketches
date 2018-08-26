2691368075f9173f77cc79be305d41a9e81d966e
Caged <justin@labratrevenge.com>
2018-08-26 09:30:31 -0700

    render a bunch of spheres that do a sin wave

--- sketches/three-joy.js
+++ sketches/three-joy.js
@@ -14,29 +14,59 @@ const settings = {
   attributes: {antialias: true} // Turn on MSAA
 }
 
-const sketch = ({context}) => {
+const sSize = 0.3
+
+const sketch = ({context, width, height}) => {
   const renderer = new THREE.WebGLRenderer({
     context
   })
+  renderer.shadowMap.enabled = true
 
   // Setup a 3D perspective camera
-  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100)
-  camera.position.set(2, 2, -4)
-  camera.lookAt(new THREE.Vector3())
+  const camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 100)
+  camera.position.set(12.524249954559526, 7.5, 11)
+  camera.lookAt(0, 0, 0)
 
   // Orbit controls for click + drag interaction
   const controls = new THREE.OrbitControls(camera)
   const scene = new THREE.Scene()
+  scene.background = new THREE.Color(0x333333)
 
-  // Add a basic ThreeJS mesh
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({
-      color: 'black',
-      wireframe: true
-    })
-  )
-  scene.add(mesh)
+  const ambientLight = new THREE.AmbientLight(0x999999)
+  scene.add(ambientLight)
+
+  const sun = new THREE.DirectionalLight(0xffffff, 1)
+  sun.position.set(-10, 10, 1)
+  sun.castShadow = true
+
+  const d = 25
+  sun.shadow.camera.left = -d
+  sun.shadow.camera.right = d
+  sun.shadow.camera.top = d
+  sun.shadow.camera.bottom = -d
+  sun.shadow.camera.near = camera.near
+  sun.shadow.camera.far = camera.far
+  sun.shadow.mapSize.x = 1024 * 2
+  sun.shadow.mapSize.y = 1024 * 2
+  scene.add(sun)
+
+  const planeGeometry = new THREE.BoxBufferGeometry(100, 0.1, 100)
+  const planeMaterial = new THREE.MeshLambertMaterial({color: 0x222222})
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+  plane.position.y = 0.0
+  plane.receiveShadow = true
+  scene.add(plane)
+
+  const spheres = []
+  for (let i = 0; i < 12; i++) {
+    const sphereGeometry = new THREE.SphereBufferGeometry(sSize, 100, 100)
+    const sphereMaterial = new THREE.MeshLambertMaterial({color: `hsl(${25 * i}, 90%, 50%)`})
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+    sphere.position.set(i / 3, sSize, 3.5 + -i / 1.5)
+    sphere.castShadow = true
+    scene.add(sphere)
+    spheres.push(sphere)
+  }
 
   return {
     // Handle window resize events
@@ -47,10 +77,15 @@ const sketch = ({context}) => {
       camera.updateProjectionMatrix()
     },
     // Render each frame
-    render({deltaTime}) {
-      // Rotate our mesh slowly
-      mesh.rotation.y += deltaTime * ((10 * Math.PI) / 180)
+    render({time}) {
+      // Rotate our camera slowly
+      camera.position.z = Math.cos(time * 0.5) * 20
+      camera.position.x = Math.sin(time * 0.5) * 20
 
+      for (const [i, s] of spheres.entries()) {
+        s.position.x = ((Math.cos(time * 5 + i) + 1) / 2) * (2 - 2 * sSize) + sSize
+        s.position.y = ((Math.sin(time * 5 + i) + 1) / 2) * (4 - 2 * sSize) + sSize
+      }
       controls.update()
       renderer.render(scene, camera)
     }
