608f9d1c0055992d11d8d9564fa6aaa29bb699ee
Caged <justin@labratrevenge.com>
2018-08-29 16:38:07 -0700

    simple sine wave field

--- sketches/canvas-sinwave-field.js
+++ sketches/canvas-sinwave-field.js
@@ -11,23 +11,19 @@ const frequency = 4
 
 const sketch = ({width, height}) => {
   const radius = 6
-  const rows = 20
-  const cols = 40
+  const rows = 25
+  const cols = 50
   const rwidth = width / cols
   const rheight = height / rows
   const points = []
-  let taperw = width
-  let taperh = height
+
   let offset = false
   for (let i = 0; i < cols; i++) {
     offset = !offset
     for (let j = 0; j < rows; j++) {
-      taperw = lerp(width / 1.5, width, j / rows)
-      taperh = lerp(height / 3, height / 2, j / rows)
-
       points.push({
-        x: lerp(0, taperw, i / cols) + rwidth / 2 + (width - taperw) / 2,
-        y: lerp(0, taperh, j / rows) + rheight / 2 + (offset ? rwidth : 0) + height / 2,
+        x: lerp(0, width, i / cols) + rwidth / 2,
+        y: lerp(0, height, j / rows) + rheight / 2 + (offset ? rwidth : 0),
         z: j,
         r: radius
       })
