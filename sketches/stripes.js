4ba83bdaac7601cee7bab32c0cbd52f625f40c94
Caged <justin@labratrevenge.com>
2018-11-18 20:45:36 -0800

    start implementing new strategy for stripes

--- sketches/stripes.js
+++ sketches/stripes.js
@@ -14,78 +14,32 @@ const settings = {
 }
 
 const generateStripes = ({
-  width = 0,
-  height = 0,
-  startX = 0,
-  startY = 0,
-  total = 3,
-  notchHeight = 45,
-  notchDegrees = 45,
-  spaceBetween = 5
+  ox = 0 /* origin x */,
+  oy = 0 /* origin y */,
+  sw = 0 /* width */,
+  sh = 0 /* height */,
+  nh = 0 /* notch height */,
+  num = 3 /* total stripes */,
+  sb = 5 /* space between stripes */,
+  bxs = 0.39 /* where to start the bottom angle */,
+  txs = 0.36 /* where to start the top angle */
 }) => {
-  const stripeData = []
-  const totalHeight = height * total
-  // const center = (i) => i *
+  const mid = lerp(0, sw, 0.5)
+  const bx = lerp(0, sw, bxs)
+  const tx = lerp(0, sw, txs)
 
-  for (let i = 0; i < total; i++) {
-    const mid = lerp(0, width, 0.5)
-    const notchHeightA = notchHeight * i
-    const notchHeightB = notchHeight * (i + 1)
-    const cutA = Math.sin(degToRad(notchDegrees)) * notchHeightA + height
-    const cutB = Math.cos(degToRad(notchDegrees)) * notchHeightB + height
-
-    const points = []
-    const y = mapRange(i, 0, total, startY, startY + totalHeight)
-    const x = startX
-    const space = i === 0 ? 0 : spaceBetween
-
-    const a = [
-      // Top left corner
-      [x, y],
-
-      [mid - cutA, y],
-      // Top right corner
-      // [x * i + width, y + space],
-      // Bottom right corner
-      [mid - cutB, y + height],
-      // Bottom left corner
-      [x * i, y + height]
-      // Point before notch
-    ]
-
-    points.push(a)
-
-    points.push([a[1], [mid, y + height]])
-
-    stripeData.push(points)
-  }
-
-  return stripeData
-}
-
-const debugPoints = (stripes, context) => {
-  const points = stripes.flat(2)
-  const pointWidth = 8
-  for (const [i, p] of points.entries()) {
-    console.log(p)
-    context.beginPath()
-    context.fillStyle = 'hsla(50, 100%, 50%, 0.8)'
-    context.strokeStyle = 'black'
-    context.arc(p[0], p[1], pointWidth, 0, Math.PI * 2)
-    context.fill()
-    context.stroke()
-
-    context.beginPath()
-    context.fillStyle = '#000000'
-    context.fillText(i + 2, p[0] - pointWidth / 2, p[1] + pointWidth / 2)
+  for (let i = 0; i < num; i++) {
+    const yb = oy + sh * i
+    const yt = oy + sh + sh * i
+    const a = [[ox, yb], [bx, yb], [tx, yt], [ox, yt]]
   }
 }
 
 const sketch = () => {
-  return ({context: ctx, width, height, canvasHeight, canvasWidth}) => {
+  return ({context: ctx, width, canvasWidth}) => {
     const sh = 100
     const sw = width
-    const nh = 50
+    const nh = 45
     const mid = lerp(0, sw, 0.5)
     const bx = lerp(0, sw, 0.39)
     const tx = lerp(0, sw, 0.36)
@@ -128,35 +82,6 @@ const sketch = () => {
     ctx.lineTo(tx, sh)
     ctx.lineTo(bx, 0)
     ctx.fill()
-
-    // Move origin to bottom left
-    // context.translate(0, height)
-    //   const stripeHeight = 100
-    //   const stripes = generateStripes({
-    //     total: 1,
-    //     width,
-    //     height: stripeHeight,
-    //     spaceBetween: 5
-    //   })
-    //   colors = shuffle(colors)
-    //
-    //   for (const [i, s] of stripes.entries()) {
-    //     for (const section of s) {
-    //       context.beginPath()
-    //       context.moveTo(...section.shift())
-    //
-    //       for (const p of section) {
-    //         context.lineTo(...p)
-    //       }
-    //
-    //       context.fillStyle = colors[i]
-    //       context.fill()
-    //     }
-    //
-    //     // context.lineTo(degToRad())
-    //   }
-    //
-    //   debugPoints(stripes, context)
   }
 }
 
